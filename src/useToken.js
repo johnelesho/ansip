e0d047839ac84a9a9e3f2791749c225d9000d156
John Elesho <eleshojohno@gmail.com>
2021-03-27 19:26:22 +0100

    Some error corrected

--- src/useToken.js
+++ src/useToken.js
@@ -3,15 +3,17 @@ import { useState } from 'react'
 export default function useToken() {
   const getToken = () => {
     const tokenString = localStorage.getItem('token')
-      const userToken = JSON.parse(tokenString)
+    return tokenString
+        //   JSON.parse(tokenString)
     //   console.log(userToken);
-    return userToken
+    // return userToken
   }
 
   const [token, setToken] = useState(getToken())
 
   const saveToken = (userToken) => {
-    localStorage.setItem('token', JSON.stringify(userToken))
+      localStorage.setItem('token', useToken)
+        //   JSON.stringify(userToken))
     setToken(userToken)
   }
 
